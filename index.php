2bd23e3a0f34d6b01a11925e7582a1a7540bb83b
David E Luna M <davidlunamontilla@gmail.com>
2022-10-19 13:47:04 -0500

    Aviso de mantenimiento

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="es-CO">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Goffice 24 | Sitio Web en Mantemiento</title>
+
+    <style>
+        :root {
+            font-family: "Open Sans", sans-serif, Arial;
+            font-size: 18px;
+        }
+
+        body {
+            margin: 0;
+            min-height: 100vh;
+        }
+
+        * {
+            box-sizing: border-box;
+        }
+
+        *::before,
+        *::after {
+            box-sizing: inherit;
+        }
+
+        .app {
+            margin: 40px auto;
+            max-width: 1200px;
+        }
+
+        hr {
+            border: 0;
+            margin: 30px auto;
+            border-bottom: 1px solid silver;
+        }
+    </style>
+</head>
+<body>
+    <main class="app" id="app">
+        <h1>Goffice 24</h1>
+        <hr>
+
+        <h2>Sitio Web en mantenimiento</h2>
+        <p>Estará disponible a las 03:00 pm</p>
+    </main>
+</body>
+</html>
